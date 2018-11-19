ff8d3516e5e04f8df31f8e23c283baee225355ae
Leonardo Spuldar Braga <spdleonardo@hotmail.com>
2018-11-19 16:49:32 -0200

    Professor

--- chamada/admin.py
+++ chamada/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Aluno, Disciplina, Curso
+from .models import Aluno, Disciplina, Curso, Professor, Chamada
 
 # Register your models here.
 admin.site.register(Aluno)
 admin.site.register(Disciplina)
 admin.site.register(Curso)
+admin.site.register(Professor)
+admin.site.register(Chamada)
+
--- chamada/models.py
+++ chamada/models.py
@@ -11,28 +11,24 @@ class Disciplina(models.Model):
         return f"{self.nome} - {self.carga_horaria} horas"
 
 
+class Pessoa(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    matricula = models.CharField(max_length=10)
+
+    class Meta:
+        abstract = True
+
+
+class Professor(Pessoa):
+    titulacao = models.CharField(max_length=50)
+
+
 class Turma(models.Model):
+    professor = models.ForeignKey(Professor, on_delete=models.DO_NOTHING, related_name="turma")
     turno = models.CharField(max_length=1)
     periodo = models.IntegerField()
     disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE, related_name="turma")
 
-
-class Aluno(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    matricula = models.CharField(max_length=10)
-    turmas = models.ManyToManyField(Turma, through='Turma_Aluno')
-
-    def __str__(self):
-        return f"{self.matricula} - {self.user.first_name}"
-
-class Turma_Aluno(models.Model):
-    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
-    turma = models.ForeignKey(Turma, on_delete=models.CASCADE)
-
-class Chamada(models.Model):
-    presenca = models.BooleanField
-    data = models.DateField
-    turma_aluno = models.ForeignKey(Turma_Aluno, on_delete=models.CASCADE)
 
 class Curso(models.Model):
     nome = models.CharField(max_length=50)
@@ -42,3 +38,23 @@ class Curso(models.Model):
 
     def __str__(self):
         return f"{self.nome}"
+
+
+class Aluno(Pessoa):
+    turmas = models.ManyToManyField(Turma, through='Turma_Aluno')
+    curso = models.ForeignKey(Curso, on_delete=models.DO_NOTHING, related_name="aluno")
+
+    def __str__(self):
+        return f"{self.matricula} - {self.user.first_name}"
+
+
+class Turma_Aluno(models.Model):
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    turma = models.ForeignKey(Turma, on_delete=models.CASCADE)
+
+
+class Chamada(models.Model):
+    presenca = models.BooleanField
+    data = models.DateField
+    turma_aluno = models.ForeignKey(Turma_Aluno, on_delete=models.CASCADE)
+
