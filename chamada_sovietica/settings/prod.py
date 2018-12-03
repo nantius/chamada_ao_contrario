739781beba84d5d1bb21ad71177df8aa3ee18c53
Leonardo Spuldar Braga <spdleonardo@hotmail.com>
2018-12-03 09:49:13 -0200

    Divisão de configs em dev/prod

--- chamada_sovietica/settings/prod.py
+++ chamada_sovietica/settings/prod.py
@@ -1,4 +1,5 @@
 from chamada_sovietica.settings.common import *
+import dj_database_url
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
@@ -6,8 +7,9 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-import dj_database_url
+django_heroku.settings(locals())
+
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
-django_heroku.settings(locals())
+
 
